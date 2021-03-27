9d978f6113543a882ccfffb044d717bbc20b3dae
Sumit Saini <sumitsaini199711@gmail.com>
2021-03-27 11:41:58 +0530

    Setting up Currencies controller

--- app/controllers/currencies_controller.rb
+++ app/controllers/currencies_controller.rb
@@ -3,8 +3,18 @@ class CurrenciesController < ApplicationController
     end
 
     def search
+        @currencies = Currency.where('Lower(name) LIKE ?',"%#{params[:search].downcase}%")
+        render json: {currencies: @currencies}
     end
 
     def calculate
+        @currency ||= Currency.find(params[:id]);
+        @amount = params[:amount];
+        render json: {
+            currency : @currency,
+            current_price : @currency.current_price,
+            amount : amount,
+            value : @currency.calculate_value(amount)
+        }
     end
 end
