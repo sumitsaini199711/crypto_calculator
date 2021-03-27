65a0880e61b6a3ce870cd6019269c87a7567d7ba
Sumit Saini <sumitsaini199711@gmail.com>
2021-03-27 11:24:53 +0530

    Adding CoinMarketCap API request inside Currency Model

--- app/models/currency.rb
+++ app/models/currency.rb
@@ -1,2 +1,22 @@
 class Currency < ApplicationRecord
+    def calculate_value(amount)
+        (current_price.to_f * amount.to_f).round(4);
+    end
+
+    def current_price
+        api_key = 'c0a0b2d6-c1eb-4ac2-89b5-c3d1b8f89a58';
+        url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?CMC_PRO_API_KEY="+api_key;
+        request = HTTParty.get(url)
+        response = JSON.parse(request.body);
+        data_response = response["data"];
+        temp = '';
+        data_response.each{ |crypto|
+            if crypto["slug"] === self.slug
+                temp = crypto["quote"]["USD"]["price"];
+                break;
+            end
+        }
+        current_price_of_crypto = temp;
+    end
+
 end
